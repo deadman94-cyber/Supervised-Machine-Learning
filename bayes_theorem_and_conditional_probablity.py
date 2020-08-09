b4b439ea1d94006608a3938b8ca32a63ae1c4bdf
deadman94-cyber <singhvineet179@gmail.com>
2020-08-09 13:24:07 +0530

    first commit

---
+++ bayes_theorem_and_conditional_probablity.py
@@ -0,0 +1,13 @@
+class new:
+    def __init__(self,pa,pb):
+        self.pa=pa
+        self.pb=pb
+    def displa(self):
+        pc=self.pb/self.pa
+        print((pc*self.pb)/self.pa,(str('%')))
+
+old = new(10,20)
+old.displa()
+
+        
+        
